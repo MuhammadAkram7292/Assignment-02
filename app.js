3344e94b50d4d91d6b0e724e26f6a434a1776733
MuhammadAkram7292 <121758916+MuhammadAkram7292@users.noreply.github.com>
2023-02-01 23:36:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,100 @@
+// Assignment 02
+// CHAPTER 02
+// Question 01
+// var a = "username";
+// console.log(a)
+
+
+// Question 02
+// var myName = "Muhammad Akram Raza"
+// console.log(myName)
+
+
+// Question 03
+// var message = "Hello World";
+// alert (message)
+// console.log(message)
+
+
+// Question 04
+// var name = "Muhammad Akram Raza";
+// alert (name)
+// // console.log(name)
+// var age = "19 Year Old";
+// alert (age)
+// // console.log(age)
+// var certification = "Certified Mobile Application Development";
+// alert (certification)
+// // console.log(certification)
+
+
+// Question 05
+// var a = "Pizza \nPizz \nPiz \nPi \nP";
+// alert (a)
+
+
+// Question 06
+// var email = "My Email address is muhammadakramraza9@gmail.com";
+// alert (email);
+
+
+// Question 07
+// var book = "A smarter way to learn JavaScript"
+// alert (book)
+
+
+// Question 08
+// document.write("<h5>Yah! I can write HTML content through Javascrip.</h>");
+
+
+
+// Chaperter 03
+
+
+// Question 01
+// var age = "I am 19 Year Old";
+// alert (age)
+
+// Question2
+// var visits = "You have visit 15 times this site !!"
+// alert(visits);
+
+// Question 3
+// var birthYear = "My birth year is 2003"
+// document.write(birthYear);
+
+// Question 4
+// var name = "Thomas";
+// var productTitle = "T-shirt ";
+// var quantity = 50;
+
+// document.write(name + productTitle + quantity)
+
+
+// Chapter 04
+
+// // question 1
+// var woman,time,money;
+// // question 2
+// // a
+
+// var berlin;
+// var professor;
+// var palermo;
+// var tokyo;
+// var helsinki;
+
+// // b 
+// // var 1ng;
+// // var ba%;
+// // var nqa8*1;
+// // var hss_8&;
+// // var hdS8/;
+
+// // Question 3 
+// var heading = "Rules for naming JS variables"
+// document.write("<h1>heading</h1>" + "\n" + "<p>Variable names can only contain a letter, $ , _ .For example $name, _name or name.</p>" + "\n" + 
+// "<p>Variable names are case sensitive.</p>" + "\n" + "<p>Variable names should not be JS keywords</p>")
+
+
+
